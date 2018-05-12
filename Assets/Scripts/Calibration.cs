2fb6d0e76281df7e84b44fb7063c560355c9d028
Unknown <alx.kntl@gmail.com>
2018-05-12 17:39:44 +0200

    Add Manual Calibration

---
+++ Assets/Scripts/Calibration.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Calibration : MonoBehaviour {
+
+    public Button left;
+    public Button right;
+    public Button up;
+    public Button down;
+
+    public Camera cam;
+
+    // Use this for initialization
+    void Start() {
+        left.onClick.AddListener(Left);
+        right.onClick.AddListener(Right);
+        up.onClick.AddListener(Up);
+        down.onClick.AddListener(Down);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void Left()
+    {
+        cam.transform.Translate(Vector3.left);
+    }
+
+    void Right()
+    {
+        cam.transform.Translate(Vector3.right);
+
+    }
+
+    void Up()
+    {
+        cam.transform.Translate(Vector3.up);
+
+    }
+    void Down()
+    {
+        cam.transform.Translate(Vector3.down);
+
+    }
+}
