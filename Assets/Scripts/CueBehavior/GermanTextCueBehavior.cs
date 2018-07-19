2bdb7f052bc3f270750b5c5f300e033a2d5b1e89
Unknown <alx.kntl@gmail.com>
2018-07-19 19:22:11 +0200

    Add German version of TextCueBehavior

---
+++ Assets/Scripts/CueBehavior/GermanTextCueBehavior.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GermanTextCueBehavior : CueBehavior
+{
+
+    public GameObject assText;
+    public GameObject refText;
+    public GameObject resText;
+    public GameObject attText;
+
+    public Text text;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void SetNeedsAssist(bool needsAssist)
+    {
+        base.SetNeedsAssist(needsAssist);
+        if (needsAssist)
+        {
+            text.text = "Hilfe";
+        }
+        else
+        {
+            SetNeutral();
+        }
+    }
+
+    protected override void SetReferencingObject(bool referencingObject)
+    {
+        base.SetReferencingObject(referencingObject);
+        if (referencingObject)
+        {
+            text.text = "Das da";
+        }
+        else
+        {
+            SetNeutral();
+        }
+    }
+
+    protected override void SetIsAttentive(bool isAttentive)
+    {
+        base.SetIsAttentive(isAttentive);
+        if (isAttentive)
+        {
+            text.text = "Nicht Aufmerksam";
+        }
+        else
+        {
+            SetNeutral();
+        }
+    }
+
+    protected override void SetExpectsResponse(bool expectsResponse)
+    {
+        base.SetExpectsResponse(expectsResponse);
+        if (expectsResponse)
+        {
+            text.text = "Du bist dran";
+        }
+        else
+        {
+            SetNeutral();
+        }
+
+    }
+
+    protected override void SetNeutral()
+    {
+        text.text = " ";
+    }
+}
